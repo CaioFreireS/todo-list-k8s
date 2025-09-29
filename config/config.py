f1359c86fab04247afbd04683f9799f1a9374fea
Caio Freire Silva <115713768+CaioFreireS@users.noreply.github.com>
2025-09-29 13:01:32 -0300

    fix: deployment.yaml

--- config/config.py
+++ config/config.py
@@ -2,15 +2,17 @@ import os
 from dotenv import load_dotenv
 import pathlib
 
-BASE_DIR = pathlib.Path(__file__).parent.resolve()
-ENV_PATH = BASE_DIR / 'db.env'
-
+BASE_DIR = pathlib.Path(__file__).parent.parent.resolve()
+ENV_PATH = BASE_DIR / 'config' / 'db.env' 
 load_dotenv(ENV_PATH) 
 
 class Config:
-    DB_HOST = os.environ.get('DB_HOST') 
+    DB_HOST = os.environ.get('DB_HOST', 'postgres-service')
     DB_NAME = os.environ.get('DB_NAME')
     DB_USER = os.environ.get('DB_USER')
     DB_PASS = os.environ.get('DB_PASS')
     
-    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+    )
